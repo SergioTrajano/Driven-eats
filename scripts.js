6ae7e66b623d2189c904dffe48201c78b2829543
Sergio Trajano <sergio.trajanosilva2001@gmail.com>
2022-04-02 14:08:50 -0300

    Envio do pedido redireciona pro whats com a mensagem dada

--- scripts.js
+++ scripts.js
@@ -40,3 +40,23 @@ function selecionaSobremessa(elemento) {
     elemento.querySelector(".icone").classList.remove("escondido");
     fecharPedido();
 }
+
+function enviar() {
+    const prato = document.querySelector(".prato").querySelector(".selecionado");
+    const nomePrato = prato.querySelector("h4").innerHTML;
+    const valorPrato = Number(prato.querySelector("span").innerHTML);
+
+    const bebida = document.querySelector(".bebida").querySelector(".selecionado");
+    const nomeBebida = bebida.querySelector("h4").innerHTML;
+    const valorBebida = Number(bebida.querySelector("span").innerHTML);
+
+    const sobremessa =document.querySelector(".sobremessa").querySelector(".selecionado");
+    const nomeSobremessa = sobremessa.querySelector("h4").innerHTML;
+    const valorSobremessa = Number(sobremessa.querySelector("span").innerHTML);
+
+    window.open("https://wa.me/5581993340979?text=" + encodeURIComponent(`Olá, gostaria de fazer o pedido:
+- Prato: ${nomePrato}
+- Bebida: ${nomeBebida}
+- Sobremesa: ${nomeSobremessa}
+Total: R$ ${(valorPrato + valorBebida + valorSobremessa).toFixed(2)}`));
+}
